88b6aa46a49fb25beba3f861e600eacad6748115
THEKOT001 <70933901+THEKOT001@users.noreply.github.com>
2020-10-08 00:48:01 +0300

    Add files via upload

---
+++ University_and_Stundents1/src/com/oop/Student.java
@@ -0,0 +1,11 @@
+package com.oop;
+
+public class Student {
+
+    public String name;
+    public int age;
+    public float mark;
+    public Student (float mark,int age,String name){
+        System.out.println(mark+" "+age+" " + " "+name);
+    }
+}
---
+++ University_and_Stundents1/src/com/oop/University.java
@@ -0,0 +1,12 @@
+package com.oop;
+
+public class University {
+    public String name;
+    public int foundationYear;
+    public Student student;
+
+    public University (String name,int anul){
+            System.out.println(name + "  " + anul);
+
+    }
+}
